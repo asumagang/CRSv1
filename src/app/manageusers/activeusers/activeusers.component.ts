2518abf37588ab4c0315e2e399b2d8e8571e377c
asumagang <janaidrhean@gmail.com>
2020-01-09 17:34:04 +0800

    last

--- src/app/manageusers/activeusers/activeusers.component.ts
+++ src/app/manageusers/activeusers/activeusers.component.ts
@@ -11,5 +11,60 @@ export class ActiveusersComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  Users=[
+    {
+      id:"1",
+      firstname:"Adrian",
+      lastname:"Sumagang",
+      email:"janaidrhean@gmail.com",
+      address:"talamban",
+      mobileno:"09356242787",
+      department:"Pantawid Pamilya"
+    },
+    {
+      id:"2",
+      firstname:"Tanjirou",
+      lastname:"Kamado",
+      email:"demonslayer@gmail.com",
+      address:"talamban",
+      mobileno:"09356242787",
+      department:"Sustainable Livelihood"
+    },
+    {
+      id:"3",
+      firstname:"Senku",
+      lastname:"Chan",
+      email:"doctorstone@gmail.com",
+      address:"talamban",
+      mobileno:"09356242787",
+      department:"Social Pension"
+    },
+    {
+      id:"4",
+      firstname:"Zenitsu",
+      lastname:"Sumagang",
+      email:"janaidrhean@gmail.com",
+      address:"talamban",
+      mobileno:"09356242787",
+      department:"Pantawid Pamilya"
+    },
+    {
+      id:"5",
+      firstname:"Ishigami",
+      lastname:"Senku",
+      email:"janaidrhean@gmail.com",
+      address:"talamban",
+      mobileno:"09356242787",
+      department:"Pantawid Pamilya"
+    },
+    {
+      id:"6",
+      firstname:"Giyu",
+      lastname:"Tomioka",
+      email:"waterhashira@gmail.com",
+      address:"talamban",
+      mobileno:"09356242787",
+      department:"Social Pension"
+    }
+  ]
 }
