b3e5a5d3fbfc560231bcc40e4538aa982f139ac3
asumagang <janaidrhean@gmail.com>
2020-01-10 13:45:45 +0800

    jan 10

--- src/app/_helpers/fake-backend.ts
+++ src/app/_helpers/fake-backend.ts
@@ -6,9 +6,9 @@ import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
 import { User, Role } from '../_models';
 
 const users: User[] = [
-    { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
-    { id: 2, username: 'user', password: 'user', firstName: 'Normal', lastName: 'User', role: Role.User },
-    { id: 3, username: 'asumagang', password: 'zayvain', firstName: 'Adrian', lastName: 'Sumagang', role: Role.User },
+    { id: 1, username: 'kcarabuena', password: 'user', firstName: 'Karen', lastName: 'Carabuena', role: Role.User },
+    { id: 2, username: 'asumagang', password: 'admin', firstName: 'Adrian', lastName: 'Sumagang', role: Role.Admin },
+    { id: 3, username: 'imraboy', password: 'pdpo', firstName: 'Ibha', lastName: 'Raboy', role: Role.PDPO},
 
 
 ];
